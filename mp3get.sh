4c9e7605069c0c36696ec9559dc21dcb8cfc4534
r3bers <mike@solovjov.net>
2016-03-09 22:47:10 +0300

    Add .sh

--- mp3get.sh
+++ mp3get.sh
@@ -5,7 +5,7 @@ killall -q wget > /dev/null
 cd ~/mp3
 DATE=`date +%Y-%m-%d-%H`
 wget -q -O ~/mp3/${DATE}.mp3 http://video.govoritmoskva.ru:8880/rufm_m.mp3 > /dev/null
-while [[ $CH = `date +%H` ]]
+while [[ ${CH} = `date +%H` ]]
 do
     DATE=`date +%Y-%m-%d-%H-%M`
     wget -q -O ~/mp3/${DATE}.mp3 http://gm.fmtuner.ru > /dev/null
